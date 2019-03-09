2742b75a08dc00aecc6a7d2c1efe1c683afd509e
jhart2 <jharttech@hotmail.com>
2019-03-08 21:24:55 -0600

    Corrected logic error.

--- digitize.sh
+++ digitize.sh
@@ -33,8 +33,13 @@ if [ "" == "$_File_Check" ]; then
 	cd Movie_Backups
 else
 	cd Videos
-	mkdir Movie_Backups
-	cd Movie_Backups
+	_File_Check_Two=$(ls | grep "Movie_Backups")
+	if [ "" == "$_File_Check_Two" ]; then
+		mkdir Movie_Backups
+		cd Movie_Backups
+	else
+		cd Movie_Backups
+	fi
 fi
 
 
@@ -68,42 +73,39 @@ while true; do
 				echo "Now going to make a digital backup of "$_MovieTitle" it will be located in ~/Videos/Movie_Backups/"
 				sleep 3
 				HandBrakeCLI -i /dev/sr0 -o "$_MovieTitle".mp4 -e x264 -q 20 -B 160
-			fi
-		fi
-	fi
-done
-	
-_CheckForFile=$(ls | grep "$_MovieTitle")
-while true; do
-	if [ "" == "$_CheckForFile" ];
-	then
-		echo "There was an error with the copying of your movie, usually this means that you need to manually specify the correct title number to encode.  I recommend trying again with a different title number.  If you do not know what this means you can read more about it at https://handbrake.fr/docs/en/1.2.0/ or you can read the manual page by running 'man HandBrakeCLI' in your linux terminal." 
-		echo -e "\n"
-		echo "Would you like to try to copy again using a different title number? y/n"
-		read _retry
-		if [ "$_retry" == "y" ];
-		then
-			echo "Please enter the number of the title to encode (If you do not know please enter '1'): "
-			read _TitleNum
-			echo "Now going to make a digital backup of "$_MovieTitle" it will be located in ~/Videos/Movie_Backups/"
-			sleep 3
-			HandbrakeCLI -i /dev/sr0 -t "$_TitleNum" -o "$_MovieTitle".mp4 -e x264 -q 20 -B 160
-			break
-		else
-			if [ "$_retry" == "n" ];
-			then
-				echo "Sorry your movie has not been copied. Thank You! - Jhart"
 				break
 			fi
 		fi
-		
-	else
-		if [ "$_MovieTitle" == "$_CheckForFile" ];
-		then
-			echo "Congratulations you now have a digital copy of your movie "$_MovieTitle".  Enjoy and Thank You! - Jhart"
-			break
-		fi
 	fi
+	_CheckForFile=$(ls | grep "$_MovieTitle")
+		if [ "" == "$_CheckForFile" ];
+		then
+			echo "There was an error with the copying of your movie, usually this means that you need to manually specify the correct title number to encode.  I recommend trying again with a different title number.  If you do not know what this means you can read more about it at https://handbrake.fr/docs/en/1.2.0/ or you can read the manual page by running 'man HandBrakeCLI' in your linux terminal." 
+			echo -e "\n"
+			echo "Would you like to try to copy again using a different title number? y/n"
+			read _retry
+			if [ "$_retry" == "y" ];
+			then
+				echo "Please enter the number of the title to encode (If you do not know please enter '1'): "
+				read _TitleNum
+				echo "Now going to make a digital backup of "$_MovieTitle" it will be located in ~/Videos/Movie_Backups/"
+				sleep 3
+				HandbrakeCLI -i /dev/sr0 -t "$_TitleNum" -o "$_MovieTitle".mp4 -e x264 -q 20 -B 160
+				break
+			else
+				if [ "$_retry" == "n" ];
+				then
+					echo "Sorry your movie has not been copied. Thank You! - Jhart"
+					break
+				fi
+			fi
+		else
+		if [ "$_MovieTitle" == "$_CheckForFile" ];
+			then
+				echo "Congratulations you now have a digital copy of your movie "$_MovieTitle".  Enjoy and Thank You! - Jhart"
+				break
+			fi
+		fi
 done
 
 
